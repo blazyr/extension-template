3948ac8c427952c69811d9e44f2cc897b2f6d45c
Luca <coduri.vitor@gmail.com>
2024-01-15 22:32:01 +0100

    using macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
 use std::rc::Rc;
 
-use spotlight_extension::{Entity, EntityType};
+use anyhow::Result;
+use spotlight_extension::{dispose, entities, entry_point, Entity, EntityType};
 
-#[no_mangle]
-fn execute() -> Option<Entity> {
-    Some(Entity {
+#[entry_point]
+fn init() -> Result<()> {
+    println!("Salut mon pote !");
+    Ok(())
+}
+
+#[entities]
+fn data() -> Result<Vec<Entity>> {
+    let entity = Entity {
         name: "test".to_string(),
         description: Some("desc".to_string()),
         alias: Some("alias".to_string()),
         command: Rc::new(Box::new(|| Ok("IT WORKS !".to_string()))),
         r#type: EntityType::Command,
-    })
+    };
+
+    Ok(vec![entity])
+}
+
+#[dispose]
+fn dispose() -> Result<()> {
+    println!("A+ mon pote !");
+    Ok(())
 }
