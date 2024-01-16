2cf5d9e9771de6df78d8cfdef70e0835eb4a48b5
Luca <coduri.vitor@gmail.com>
2024-01-16 21:38:18 +0100

    added result type on function return

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use abi_stable::{
     prefix_type::PrefixTypeTrait,
     sabi_extern_fn,
     std_types::{
+        RBoxError,
         ROption::{RNone, RSome},
+        RResult::{self, RErr, ROk},
         RVec,
     },
 };
@@ -14,36 +16,59 @@ fn instantiate_root_module() -> Plugin_Ref {
     Plugin {
         entities,
         on_entity_action,
+        on_mount,
+        on_dispose,
     }
     .leak_into_prefix() // Converts the `MinMod` into `MinMod_Ref` and leaks it
 }
 
 #[sabi_extern_fn]
-pub fn entities() -> RVec<REntity> {
-    vec![
+pub fn entities() -> RResult<RVec<REntity>, RBoxError> {
+    ROk(vec![
         REntity {
             id: 0,
-            name: RSome("Firefox".into()),
+            name: "Firefox".into(),
             description: RSome("Browser".into()),
             alias: RNone,
         },
         REntity {
             id: 1,
-            name: RSome("Chrome".into()),
+            name: "Chrome".into(),
             description: RSome("Browser".into()),
             alias: RNone,
         },
         REntity {
             id: 2,
-            name: RSome("Vivaldi".into()),
+            name: "Vivaldi".into(),
             description: RSome("Browser".into()),
             alias: RNone,
         },
     ]
-    .into()
+    .into())
+}
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DataStoreError {
+    #[error("data store disconnected")]
+    Disconnect,
+    #[error("unknown data store error")]
+    Unknown,
 }
 
 #[sabi_extern_fn]
-pub fn on_entity_action(id: u64) {
+pub fn on_mount() -> RResult<(), RBoxError> {
+    RErr(RBoxError::new(DataStoreError::Disconnect))
+}
+
+#[sabi_extern_fn]
+pub fn on_dispose() -> RResult<(), RBoxError> {
+    RErr(RBoxError::new(DataStoreError::Unknown))
+}
+
+#[sabi_extern_fn]
+pub fn on_entity_action(id: u64) -> RResult<(), RBoxError> {
     println!("ENTITY ACTION: {}", id);
+    ROk(())
 }
