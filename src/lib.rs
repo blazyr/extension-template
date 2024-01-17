8dc1522c3a58e57dfb5732092b636ad8cdfd03ef
Luca <coduri.vitor@gmail.com>
2024-01-17 20:17:59 +0100

    Using the builder pattern

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,20 @@ use abi_stable::{
     sabi_extern_fn,
     std_types::{
         RBoxError,
-        ROption::{RNone, RSome},
         RResult::{self, RErr, ROk},
         RVec,
     },
 };
 use spotlight_extension::{Plugin, Plugin_Ref, REntity};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DataStoreError {
+    #[error("data store disconnected")]
+    Disconnect,
+    #[error("unknown data store error")]
+    Unknown,
+}
 
 #[export_root_module]
 fn instantiate_root_module() -> Plugin_Ref {
@@ -25,36 +33,11 @@ fn instantiate_root_module() -> Plugin_Ref {
 #[sabi_extern_fn]
 pub fn entities() -> RResult<RVec<REntity>, RBoxError> {
     ROk(vec![
-        REntity {
-            id: 0,
-            name: "Firefox".into(),
-            description: RSome("Browser".into()),
-            alias: RNone,
-        },
-        REntity {
-            id: 1,
-            name: "Chrome".into(),
-            description: RSome("Browser".into()),
-            alias: RNone,
-        },
-        REntity {
-            id: 2,
-            name: "Vivaldi".into(),
-            description: RSome("Browser".into()),
-            alias: RNone,
-        },
+        REntity::builder(0, "Firefox").build(),
+        REntity::builder(1, "Google Chrome").build(),
+        REntity::builder(2, "Vivaldi").build(),
     ]
     .into())
-}
-
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum DataStoreError {
-    #[error("data store disconnected")]
-    Disconnect,
-    #[error("unknown data store error")]
-    Unknown,
 }
 
 #[sabi_extern_fn]
